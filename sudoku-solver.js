d7eb44817a4ef549723f3dbf4cd7d98a22c618fd
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-12-17 23:51:47 -0500

    feat: isolate state

--- sudoku-solver.js
+++ sudoku-solver.js
@@ -1,34 +1,36 @@
 'use strict'
 
 module.exports = function (input) {
-  const cells = new Array(9).fill(0).map(_ => {
-    return new Array(9).fill(0).map(_ => [1, 2, 3, 4, 5, 6, 7, 8, 9])
-  })
-  const guesses = []
-  let remaining = 81
+  const state = {
+    cells: new Array(9).fill(0).map(_ => {
+      return new Array(9).fill(0).map(_ => [1, 2, 3, 4, 5, 6, 7, 8, 9])
+    }),
+    guesses: [],
+    remaining: 81
+  }
 
-  const remove = (x, y, digit) => {
-    const cell = cells[y][x]
+  const remove = (state, x, y, digit) => {
+    const cell = state.cells[y][x]
     if (Array.isArray(cell) && cell.includes(digit)) {
       const newCell = cell.filter(value => value !== digit)
       if (newCell.length === 1) {
-        guesses.push({ x, y, digit: newCell[0] })
+        state.guesses.push({ x, y, digit: newCell[0] })
       }
-      cells[y][x] = newCell
+      state.cells[y][x] = newCell
     }
   }
 
-  const set = (x, y, digit) => {
-    cells[y][x] = digit
-    --remaining
+  const set = (state, x, y, digit) => {
+    state.cells[y][x] = digit
+    --state.remaining
     const gxBase = Math.floor(x / 3) * 3
     const gyBase = Math.floor(y / 3) * 3
     for (let z = 0; z < 9; ++z) {
-      remove(z, y, digit)
-      remove(x, z, digit)
+      remove(state, z, y, digit)
+      remove(state, x, z, digit)
       const gx = gxBase + z % 3
       const gy = gyBase + Math.floor(z / 3)
-      remove(gx, gy, digit)
+      remove(state, gx, gy, digit)
     }
   }
 
@@ -36,18 +38,18 @@ module.exports = function (input) {
     for (let x = 0; x < 9; ++x) {
       const digit = input[y][x]
       if (digit !== '.') {
-        set(x, y, Number(digit))
+        set(state, x, y, Number(digit))
       }
     }
   }
 
-  while (guesses.length) {
-    const { x, y, digit } = guesses.shift()
-    set(x, y, digit)
+  while (state.guesses.length) {
+    const { x, y, digit } = state.guesses.shift()
+    set(state, x, y, digit)
   }
 
-  if (remaining !== 0) {
-    console.log('Missing', remaining, 'cells')
+  if (state.remaining !== 0) {
+    console.log('Missing', state.remaining, 'cells')
     for (let y = 0; y < 9; ++y) {
       if (y === 3 || y === 6) {
         console.log('---+---+---')
@@ -57,8 +59,8 @@ module.exports = function (input) {
         if (x === 3 || x === 6) {
           output.push('|')
         }
-        if (Array.isArray(cells[y][x])) {
-          output.push(cells[y][x].length)
+        if (Array.isArray(state.cells[y][x])) {
+          output.push(state.cells[y][x].length)
         } else {
           output.push(' ')
         }
@@ -67,5 +69,5 @@ module.exports = function (input) {
     }
   }
 
-  return cells
+  return state.cells
 }
