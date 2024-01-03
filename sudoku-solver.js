492192c98be3d6da4717bdea344bc30a864db369
ArnaudBuchholz <arnaud.buchholz@free.fr>
2024-01-03 00:38:21 -0500

    feat(solver): iterate over possibilities

--- sudoku-solver.js
+++ sudoku-solver.js
@@ -1,7 +1,7 @@
 'use strict'
 
 module.exports = function (input) {
-  const state = {
+  const initialState = {
     cells: new Array(9).fill(0).map(_ => {
       return new Array(9).fill(0).map(_ => [1, 2, 3, 4, 5, 6, 7, 8, 9])
     }),
@@ -34,7 +34,7 @@ module.exports = function (input) {
     }
   }
 
-  const choices = (state, x, y) => {
+  const getChoices = (state, x, y) => {
     const cell = state.cells[y][x]
     if (Array.isArray(cell)) {
       return cell
@@ -42,58 +42,80 @@ module.exports = function (input) {
     return []
   }
 
+  const clone = state => {
+    const cells = new Array(9).fill(0).map((_, y) => {
+      return new Array(9).fill(0).map((_, x) => {
+        const cell = state.cells[y][x]
+        if (Array.isArray(cell)) {
+          return [...cell]
+        }
+        return cell
+      })
+    })
+    return {
+      cells,
+      guesses: [],
+      remaining: state.remaining
+    }
+  }
+
   // Set the puzzle input
   for (let y = 0; y < 9; ++y) {
     for (let x = 0; x < 9; ++x) {
       const digit = input[y][x]
       if (digit !== '.') {
-        set(state, x, y, Number(digit))
+        set(initialState, x, y, Number(digit))
       }
     }
   }
 
-  // Removes obvious guesses
-  while (state.guesses.length) {
-    const { x, y, digit } = state.guesses.shift()
-    set(state, x, y, digit)
-  }
+  const states = [
+    clone(initialState)
+  ]
 
-  if (state.remaining !== 0) {
-    console.log('Still missing', state.remaining, 'cells')
+  while (states.length) {
+    console.log(states.length)
+    const state = states.pop()
 
-    const remaining = []
+    while (state.guesses.length) {
+      const { x, y, digit } = state.guesses.shift()
+      set(state, x, y, digit)
+    }
+
+    if (state.remaining === 0) {
+      return state.cells
+    }
+
+    const remainingChoices = []
     for (let y = 0; y < 9; ++y) {
-      if (y === 3 || y === 6) {
-        console.log('---+---+---')
-      }
-      const output = []
       for (let x = 0; x < 9; ++x) {
-        if (x === 3 || x === 6) {
-          output.push('|')
-        }
-        const cell = choices(state, x, y)
-        if (cell.length > 0) {
+        const choices = getChoices(state, x, y)
+        if (choices.length > 0) {
           let score = 0
           const gxBase = Math.floor(x / 3) * 3
           const gyBase = Math.floor(y / 3) * 3
           for (let z = 0; z < 9; ++z) {
-            score += choices(state, z, y).length + choices(state, x, z).length
+            score += getChoices(state, z, y).length + getChoices(state, x, z).length
             const gx = gxBase + z % 3
             const gy = gyBase + Math.floor(z / 3)
-            score += choices(state, gx, gy).length
+            score += getChoices(state, gx, gy).length
           }
-          remaining[cell.length] ??= []
-          remaining[cell.length].push({ x, y, choices: cell, score })
-          output.push(cell.length)
-        } else {
-          output.push(' ')
+          remainingChoices.push({ x, y, choices, score })
         }
       }
-      console.log(output.join(''))
     }
+    remainingChoices.sort((first, second) => {
+      if (first.choices.length !== second.choices.length) {
+        return first.choices.length - second.choices.length
+      }
+      return first.score - second.score
+    })
 
-    console.log(remaining)
+    const { x, y, choices } = remainingChoices[0]
+    choices.forEach(digit => {
+      const clonedState = clone(state)
+      clonedState.guesses.push({ x, y, digit })
+      states.push(clonedState)
+    })
   }
-
-  return state.cells
 }
