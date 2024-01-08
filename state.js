68a19158f6c4d03784362d4d75497428400a9d7b
ArnaudBuchholz <arnaud.buchholz@free.fr>
2024-01-08 13:58:42 -0500

    feat(state): wip

--- state.js
+++ state.js
@@ -1,18 +1,43 @@
 'use strict'
 
-import { all } from './cell'
+import { chunk, horizontal, vertical } from './axis'
+import { all, unset, resolve } from './cell'
 
 module.exports = {
   alloc (clone = undefined) {
     if (clone) {
       return {
         ...clone,
-        cells: [...clone.cells]
+        cells: [...clone.cells],
+        changes: [...clone.changes]
       }
     }
     return {
       cells: new Array(81).fill(all),
-      remaining: 81
+      remaining: 81,
+      changes: []
+    }
+  },
+
+  applyChanges (state) {
+    while (state.changes.length > 0) {
+      const { coord, digit } = state.changes.shift()
+      state.cells[coord] = resolve(state.cells[coord], digit)
+      for (let x of horizontal(coord)) {
+        if (x !== coord) {
+          state.cells[x] = unset(state.cells[x], digit)
+        }
+      }
+      for (let y of vertical(coord)) {
+        if (y !== coord) {
+          state.cells[y] = unset(state.cells[y], digit)
+        }
+      }
+      for (let c of chunk(coord)) {
+        if (c !== coord) {
+          state.cells[c] = unset(state.cells[c], digit)
+        }
+      }
     }
   }
 }
--- state.spec.js
+++ state.spec.js
@@ -1,22 +1,34 @@
 'use strict'
 
 import { describe, it, expect } from 'vitest'
-import { alloc } from './state'
-import { all, unset } from './cell'
+import { alloc, applyChanges } from './state'
+import { all, unset, readDigits } from './cell'
 
 describe('allocate a new state', () => {
   it('initializes members', () => {
     const state = alloc()
     expect(state.cells).toStrictEqual(new Array(81).fill(all))
     expect(state.remaining).toBe(81)
+    expect(Array.isArray(state.changes)).toBe(true)
+    expect(state.changes.length).toBe(0)
   })
 
   it('clones a state', () => {
     const state = alloc()
     state.cells[0] = unset(state.cells[0], 5)
     state.remaining = 123
+    state.changes = [{ coord: 0, digit: 4 }]
 
     const clone = alloc(state)
     expect(clone).toStrictEqual(state)
+  })
+})
+
+describe('changes', () => {
+  it('apply changes and update the cells', () => {
+    const state = alloc()
+    state.changes.push({ coord: 0, digit: 5 })
+    applyChanges(state)
+    expect([...readDigits(state.cells[0])]).toStrictEqual([1, 2, 3, 4, 6, 7, 8, 9])
   })
 })
